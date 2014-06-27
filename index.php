e91ff8e95913253bba6221cb44c1e938ead934ab
me <me@here.com>
2014-06-27 17:15:22 +0800

    Converted JSON Data to Array

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Authorization: <KEY>', 'Content-type: application/json'));
 $result=curl_exec ($ch);
-echo $result;
+$json = json_decode($result, TRUE);
+var_dump($json);
 curl_close ($ch);
 ?>
 </body>
